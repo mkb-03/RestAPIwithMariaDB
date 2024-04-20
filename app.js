cd85eda50c878b00e26e1f49563eecc6a8cddd26
Muhammad Khizran <muhammadkhizranbutt@gmail.com>
2024-04-21 00:57:12 +0500

    Learning RestAPI with Express and MariaDB

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/person', (req, res) => {
+  
+})
+app.post('/person', (req, res) => {
+  
+})
+app.update('/person', (req, res) => {
+  
+})
+app.delete('/person', (req, res) => {
+  
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
