516890b94c1717b8eaa54e9fd026879ed0964f72
Muhammad Khizran <muhammadkhizranbutt@gmail.com>
2024-04-21 14:13:38 +0500

    implementing get API

--- database.js
+++ database.js
@@ -8,3 +8,17 @@ const pool = mariaDB.createPool({
     database : 'mydb'
 
 })
+
+exports.getPerson = async function (){
+    let conn 
+    try {
+        conn = await pool.getConnection()
+        const rows = await conn.query("SELECT * FROM person")
+        return rows
+
+    } catch (err) {
+        throw err
+    } finally {
+        if(conn) conn.end() // release pool
+    }
+}
