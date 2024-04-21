edac7804d336a18b3947adc08294c6eb2fdc9ccf
Muhammad Khizran <muhammadkhizranbutt@gmail.com>
2024-04-21 14:33:26 +0500

    implemented post API for person

--- database.js
+++ database.js
@@ -22,3 +22,20 @@ exports.getPerson = async function (){
         if(conn) conn.end() // release pool
     }
 }
+
+// POST request query 
+
+exports.addPerson = async function (params){
+    let {name, age, salary} = params
+    let conn 
+    try {
+        conn = await pool.getConnection()
+        const res = await conn.query("INSERT INTO person (name, age, salary) VALUES (?,?,?) ", [name , age, salary])
+        return {id_person: parseInt(res.inertId)}
+
+    } catch (err) {
+        throw err
+    } finally {
+        if(conn) conn.end() // release pool
+    }
+}
