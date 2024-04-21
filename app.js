dba847a7ac6b80b309822bc75597acd5b806adb3
Muhammad Khizran <muhammadkhizranbutt@gmail.com>
2024-04-21 14:23:57 +0500

    nimplementing post API

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const database = require('./database')
 
 const app = express()
 const port = 3000
-
+app.use(express.json())
 app.get('/person', async(req, res) => {
   const result = await database.getPerson()
   res.json(result)
 })
 
 
-app.post('/person', (req, res) => {
-  
+app.post('/person', async(req, res) => {
+    const body = req.body
+    const result = await database.addPerson(body)
+    res.json(result)
 })
-app.update('/person', (req, res) => {
+app.put('/person', (req, res) => {
   
 })
 app.delete('/person', (req, res) => {
@@ -22,5 +24,5 @@ app.delete('/person', (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
